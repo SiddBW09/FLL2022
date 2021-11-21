47cfed81153d77a1c7d96784dcb9a7b1d5a69f88
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2021-11-21 12:57:20 -0800

    Create truck.py

---
+++ truck.py
@@ -0,0 +1,13 @@
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_B,MoveTank, SpeedPercent, follow_for_ms, MoveSteering
+from ev3dev2.sensor.lego import ColorSensor, GyroSensor
+import Navigation
+from ev3dev2.motor import MediumMotor
+
+def code():
+  #Sample Code
+  
+
+#Execute
+if __name__ == "__main__":
+    code()
