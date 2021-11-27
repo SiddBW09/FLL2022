ba6c7bbfeba4b04c97cb17940531dbf65d83d2cb
SiddBW <zorg4001@gmail.com>
2021-11-27 15:15:38 -0800

    Updating

--- Sorter.py
+++ Sorter.py
@@ -13,6 +13,7 @@ import claw
 my_tank = MoveTank(OUTPUT_A, OUTPUT_B)
 lift = MediumMotor(OUTPUT_D)
 claw = claw.Claw()
+claw.claw.reset()
  # Init Color Sensors
 
 colorRight = ColorSensor(INPUT_2)
@@ -20,31 +21,36 @@ colorRight = ColorSensor(INPUT_2)
 colorLeft = ColorSensor(INPUT_3)
 
 
-
 #Moving the Robot
-claw.claw_open(100)
-lift.on_for_rotations(49, 3)
-claw.claw_close(100)
+for x in range(0, 3):
+    claw.claw_close(100)
+'''
 lift.on_for_rotations(49, -3)
 sleep(0.2)
 my_tank.on_for_rotations(20, 20, -.5)
-my_tank.on_for_rotations(20, 0, 0.85)
+my_tank.on_for_rotations(20, 0, 0.8)
 my_tank.on_for_rotations(20, 20, 0.8)
 sleep(1)
 lift.on_for_rotations(49, 2.5)
-claw.claw_open(100)
-lift.on_for_rotations(49, -3)
-my_tank.on_for_rotations(20, 20, -0.6)
-my_tank.on_for_rotations(-15, 15, 0.43)
-my_tank.on_for_rotations(20, 20, 0.5)
-lift.on_for_rotations(49, 3)
-claw.claw_close(100)
-lift.on_for_rotations(49,-3)
-my_tank.on_for_rotations(10, 10, -.55)
-my_tank.on_for_rotations(15, -15, 0.43)
-my_tank.on_for_rotations(15, 15, 0.5)
-sleep(0.5)
-lift.on_for_rotations(49, 3)
-sleep(1)
-claw.claw_open(100)
 
+claw.claw_open(100)
+# lift.on_for_rotations(49, -3)
+# my_tank.on_for_rotations(20, 20, -0.6)
+# my_tank.on_for_rotations(-15, 15, 0.43)
+# my_tank.on_for_rotations(20, 20, 0.5)
+# lift.on_for_rotations(-49, 3)
+
+# claw.claw_close(100)
+
+
+
+# lift.on_for_rotations(49, 3)
+
+# my_tank.on_for_rotations(10, 10, -.55)
+# my_tank.on_for_rotations(15, -15, 0.43)
+# my_tank.on_for_rotations(15, 15, 0.5)
+# lift.on_for_rotations(49, 3)
+
+
+# claw.claw_close(100)
+'''
