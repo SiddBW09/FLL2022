51cd786c0571f3ecfaa9996cadbf905b5a66ef98
SiddBW <zorg4001@gmail.com>
2021-11-27 15:15:25 -0800

    Updating

--- claw.py
+++ claw.py
@@ -12,22 +12,25 @@ class Claw():
         self.claw = MediumMotor(OUTPUT_C)
 
     def claw_open(self, percent):
-
-        self.claw.on_for_rotations(80, 3)
-
-
+        self.claw.reset()
+        self.claw.on_for_rotations(-30, 0.1)
 
     def claw_close(self, percent):
         i = 0
+        self.claw.reset()
+        while True:
 
-        while i < 11:
-            self.claw.on_for_rotations(-80, 5, brake = False)
+            self.claw.on_for_rotations(10, 0.01, brake = False)
 
-
+            init.debug_print(self.claw.state)
             if len(self.claw.state) > 1:
                 init.debug_print("Stalled")
-                i = 11
+                break
+            sleep(0.5)
             i += 1
+        init.debug_print(i)
+        return
+
 
     # def claw_open(self, percent):
     #     rotations = 3 * (percent/100)
