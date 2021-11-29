1860dc2d0c403a656722f89badd4f16bf8336882
SiddBW <zorg4001@gmail.com>
2021-11-28 16:35:45 -0800

    adding

--- Sorter.py
+++ Sorter.py
@@ -9,50 +9,99 @@ from time import sleep
 from ev3dev2.sound import Sound
 import init
 import claw
-
-my_tank = MoveTank(OUTPUT_A, OUTPUT_B)
-lift = MediumMotor(OUTPUT_D)
-claw = claw.Claw()
-claw.claw.reset()
- # Init Color Sensors
-
-colorRight = ColorSensor(INPUT_2)
-
-colorLeft = ColorSensor(INPUT_3)
+import Navigation
 
 
-#Moving the Robot
-if claw.claw_close(100):
-    #holding somethigng
-else:
-    # not holding
-'''
-lift.on_for_rotations(49, -3)
-sleep(0.2)
-my_tank.on_for_rotations(20, 20, -.5)
-my_tank.on_for_rotations(20, 0, 0.8)
-my_tank.on_for_rotations(20, 20, 0.8)
-sleep(1)
+
+def current_run():
+
+    init.debug_print("In current run")
+    my_tank = Navigation.tank_init()
+    lift = MediumMotor(OUTPUT_D)
+    MyClaw = claw.Claw()
+    MyClaw.claw.reset()
+    Have_brick(lift)
+
+
+
+
+
+
+def SlotToCircle(x):
+    #move to
+    my_tank.on_for_rotations(15, 15, -0.3)
+    my_tank.on_for_rotations(15, 0, 0.85)
+    my_tank.on_for_rotations(25, 25, x)
+    lift.on_for_rotations(49, 3)
+    sleep(0.5)
+    claw.claw_open(100)
+
+def CircleToSlot():
+    my_tank.on_for_rotations(15, 15, -0.75)
+    my_tank.on_for_rotations(-15, 15, 0.5)
+    my_tank.on_for_rotations(15, 15, 0.2)
+
+
+def no_detection():
+    claw.claw_open(100)
+    lift.on_for_rotations(49, -3)
+    my_tank.on_for_rotations(15, 15, 0.3)
+    claw.claw_open(100)
+    lift.on_for_rotations(49, 3)
+    claw.claw_close(100)
+    lift.on_for_rotations(49, -3)
+
+
+def Have_brick(lift):
+
+    init.debug_print("Have brick")
+    i = 0
+    lift.reset()
+    while True:
+        init.debug_print("In loop")
+        lift.on_for_rotations(5, 0.1, brake=False)
+        init.debug_print(lift.state)
+        if 'stalled' in lift.state:
+            init.debug_print(lift.state)
+            break
+        i += 1
+    init.debug_print(i)
+    if (i > 15):
+     return False
+    return True
+
+
+
+
+
+#
+'''claw.claw_open(100)
 lift.on_for_rotations(49, 2.5)
+if claw.claw_close(100):
+    lift.on_for_rotations(49, -3)
+    SlotToCircle(0.85)
+    lift.on_for_rotations(49, 3)
+    sleep(1)
+    claw.claw_open(100)
+    sleep(0.5)
+    lift.on_for_rotations(49, -3)
+    CircleToSlot()
+else:
+    no_detection()
+    my_tank.on_for_rotations(15, 15, -0.3)
+    SlotToCircle(0.85)'''
 
+
+'''my_tank.on_for_rotations(15, 15, 0.15)
 claw.claw_open(100)
-# lift.on_for_rotations(49, -3)
-# my_tank.on_for_rotations(20, 20, -0.6)
-# my_tank.on_for_rotations(-15, 15, 0.43)
-# my_tank.on_for_rotations(20, 20, 0.5)
-# lift.on_for_rotations(-49, 3)
-
-# claw.claw_close(100)
-
+sleep(0.5)
+lift.on_for_rotations(49, 3)
+sleep(0.5)
+claw.claw_close(100)
+sleep(0.5)
+lift.on_for_rotations(49, -3)'''
 
 
-# lift.on_for_rotations(49, 3)
-
-# my_tank.on_for_rotations(10, 10, -.55)
-# my_tank.on_for_rotations(15, -15, 0.43)
-# my_tank.on_for_rotations(15, 15, 0.5)
-# lift.on_for_rotations(49, 3)
+current_run()
 
 
-# claw.claw_close(100)
-'''
