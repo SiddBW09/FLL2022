b69d7a0860acf40300d6316b2d2838cd01f900f0
Pranav Dasan <62214486+pranavdasan@users.noreply.github.com>
2021-12-08 15:16:46 -0800

    Update

--- NorthWestSide.py
+++ NorthWestSide.py
@@ -9,6 +9,7 @@ from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_B,MoveTank, Spe
 from ev3dev2.sensor.lego import ColorSensor, GyroSensor
 import Navigation
 from time import sleep
+import time
 from ev3dev2.motor import OUTPUT_B, MediumMotor
 import Navigation
 import init
@@ -19,10 +20,24 @@ def Coachie_Code():
     lift = MediumMotor(OUTPUT_D)
     Claw = claw.Claw()
     tank.gyro.reset()
+    start_time = time.time()
 
+    '''
+    Testy Test test
+    for x in range(0, 4):
+        tank.turn_degrees(10, -90, True, 1)
+        Navigation.gyro_check(tank, 10, -90)
+        init.debug_print(tank.gyro.angle)
+    '''
+
+    lift.on_for_rotations(-10, 0.1)
+    #Lift up at start
 
     # Blue thing mission
-    Navigation.distance_to_object(tank, 28, "Forward", 20)
+    Claw.claw_open(100)
+
+    #Go to blue mission thing
+    Navigation.distance_to_object(tank, 30, "Forward", 20)
 
     tank.on_for_rotations(0, 10, 0.8)
     tank.on_for_rotations(0, -10, 0.8)
@@ -31,59 +46,59 @@ def Coachie_Code():
 
     Navigation.distance_to_object(tank, 25, "Forward", 10)
 
+    #Do the blue mission thing
     tank.turn_degrees(10, 35, True, 1)
 
 
     Navigation.gyro_check(tank, 5, 35)
 
-
     #Go to SwitchE
     Navigation.distance_to_object(tank, 35, "Forward", 10)
 
-    Claw.claw_open(100)
+    Claw.claw_open(30)
 
-    sleep(2)
     tank.turn_degrees(10, 15, True, 1)
     Navigation.distance_to_object(tank, 8, "Forward", 10)
 
     #OG value 5
-    tank.turn_degrees(10, 8, True, 1)
-    Claw.claw_close(100)
-
+    Navigation.gyro_check(tank, 5, 65)
+    Claw.claw.on_for_rotations(50, 1)
 
 
     #Do SwitchE mission
-    lift.on_for_rotations(-30, 2)
-
-    return
-
+    lift.on_for_rotations(-50, 3)
     Claw.claw_close(100)
 
-    #Go back to orange planey
+
+    #Go back to home
     tank.turn_degrees(5, -5, True, 1)
     Navigation.distance_to_object(tank, 8, "Backward", 10)
     tank.turn_degrees(5, -15, True, 1)
-    Navigation.distance_to_object(tank, 25, "Backward", 10)
+    Navigation.distance_to_object(tank, 60, "Backward", 10)
+    Navigation.gyro_check(tank, 5, 35)
 
+    #Turn to 0
+    tank.turn_degrees(10, -35, True, 1)
+    Navigation.gyro_check(tank, 5, 0)
 
-    #Turn to Green thing
-    tank.turn_degrees(20, -60, True, 1)
-    Navigation.gyro_check(tank, 5, -25)
+    #Go at 0
+    Navigation.distance_to_object(tank, 26, "Forward", 10)
 
-    #Complete Green mission
-    Navigation.distance_to_object(tank, 9, "Forward", 10)
+    #Turn to perpendicular and approach
+    tank.turn_degrees(10, 47, True, 1)
+    #Navigation.distance_to_object(tank, 10, "Forward", 10)
+
+    #Go to Cargo Plane and do mission
+    Navigation.distance_to_object(tank, 6, "Forward", 10)
+    Claw.claw_open(100)
+    lift.on_for_rotations(100, 6)
     Claw.claw_close(100)
-    lift.on_for_rotations(35, 1)
-    return
-
-
-
-    return
-
-
-
-    return
-
+    tank.turn_degrees(10, 27)
+    Navigation.gyro_check(tank, 10, 27)
+    lift.on_for_rotations(-30, 6)
+    Claw.claw_open(100)
+    Navigation.distance_to_object(tank, 5, "Forward", 10)
+    lift.on_for_rotations(30, 6)
 
 
 #Execute Northide Missions
