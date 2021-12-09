88ffd2de61bbe552156f2aa12a0ef7a87d9d8f3c
rdasan <pranavdasan8@gmail.com>
2021-12-08 21:13:36 -0800

    q

--- NorthWestSide.py
+++ NorthWestSide.py
@@ -42,7 +42,7 @@ def Coachie_Code():
     tank.on_for_rotations(0, 10, 0.8)
     tank.on_for_rotations(0, -10, 0.8)
     Navigation.gyro_check(tank, 5, 0)
-    init.debug_print(tank.gyro.angle)
+
 
     Navigation.distance_to_object(tank, 25, "Forward", 10)
 
@@ -53,12 +53,13 @@ def Coachie_Code():
     Navigation.gyro_check(tank, 5, 35)
 
     #Go to SwitchE
-    Navigation.distance_to_object(tank, 35, "Forward", 10)
+    Navigation.distance_to_object(tank, 30, "Forward", 10)
 
     Claw.claw_open(30)
 
     tank.turn_degrees(10, 15, True, 1)
-    Navigation.distance_to_object(tank, 8, "Forward", 10)
+    Navigation.distance_to_object(tank, 10, "Forward", 10)
+    tank.turn_degrees(5, 3, True, 1)
 
     #OG value 5
     Navigation.gyro_check(tank, 5, 65)
@@ -74,31 +75,52 @@ def Coachie_Code():
     tank.turn_degrees(5, -5, True, 1)
     Navigation.distance_to_object(tank, 8, "Backward", 10)
     tank.turn_degrees(5, -15, True, 1)
-    Navigation.distance_to_object(tank, 60, "Backward", 10)
+    Navigation.distance_to_object(tank, 42, "Backward", 10)
     Navigation.gyro_check(tank, 5, 35)
 
     #Turn to 0
     tank.turn_degrees(10, -35, True, 1)
     Navigation.gyro_check(tank, 5, 0)
 
+    #GO at 0
+    Navigation.distance_to_object(tank, 15, "Forward", 10)
+
+    #Turn perpendicular to Cargo Plane
+    tank.turn_degrees(10, 32, True, 1)
+
+    lift.on_for_rotations(100, 6, True)
+
+    return
+
+    #Turn to perpendicular and approach
+    tank.turn_degrees(10, 32, True, 1)
+    Navigation.gyro_check(tank, 5, 32)
+
+    #Go to Cargo Plane and do mission
+    lift.on_for_rotations(30, 6)
+    return
+
     #Go at 0
     Navigation.distance_to_object(tank, 26, "Forward", 10)
+    Navigation.gyro_check(tank, 5, 0)
 
     #Turn to perpendicular and approach
     tank.turn_degrees(10, 47, True, 1)
+    Navigation.gyro_check(tank, 5, 50)
     #Navigation.distance_to_object(tank, 10, "Forward", 10)
 
     #Go to Cargo Plane and do mission
-    Navigation.distance_to_object(tank, 6, "Forward", 10)
     Claw.claw_open(100)
+    lift.on_for_rotations(100, 6, True)
+    lift.on_for_rotations(-100, 5, True)
+    tank.turn_degrees(10, 25, True, 1)
+    Navigation.distance_to_object(tank, 10, "Forward")
     lift.on_for_rotations(100, 6)
-    Claw.claw_close(100)
-    tank.turn_degrees(10, 27)
-    Navigation.gyro_check(tank, 10, 27)
-    lift.on_for_rotations(-30, 6)
-    Claw.claw_open(100)
-    Navigation.distance_to_object(tank, 5, "Forward", 10)
-    lift.on_for_rotations(30, 6)
+    Navigation.distance_to_object(tank, 8, "Backward")
+    lift.on_for_rotations(-100, 6)
+    Navigation.distance_to_object(tank, 10, "Backward")
+
+
 
 
 #Execute Northide Missions
