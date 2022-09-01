aa27f6e73810d2c781fb6507263891b3ef6dfa4a
LittleLightning <sahana@samrat.us>
2022-08-31 18:30:49 -0700

    Cleaned Up Navigation

--- Navigation.py
+++ Navigation.py
@@ -29,121 +29,6 @@ def tank_init():
 '''
 Distance Goer
 '''
-#Goes to certain distance
-def distance_to_object(tank, distance, direction, speed=10):
-
-    distance_not_reached = True
-
-    #Number of wheel rotations needed to get there
-    number_of_wheel_rotations = distance / 25.6353961
-
-    rotation = number_of_wheel_rotations / 10
-
-    if rotation < 0.3:
-        rotation = 0.2
-
-    sleep(0.5)
-    #The angle the robot started with
-    inital_angle = tank.gyro.angle
-
-    while number_of_wheel_rotations > 0:
-
-        if (rotation > number_of_wheel_rotations):
-            rotation = number_of_wheel_rotations
-
-        if direction == "Forward":
-            tank.on_for_rotations(speed, speed, rotation, brake=False, block=True)
-        if direction == "Backward":
-            tank.on_for_rotations(-speed, -speed, rotation, brake=False, block=True)
-
-
-        #Minus current rotation from total rotations needed
-        number_of_wheel_rotations -= rotation
-
-        #See's if robot veered of track, if so fixes it
-        degrees_off = inital_angle - tank.gyro.angle
-
-        tank.turn_degrees(10, degrees_off)
-'''
-Line Following Program:
-'''
-def Line_Following(tank, colorLeft, colorRight, distance):
-    #Saying to use the global variables, and not def variables
-    global speed
-
-    distance_not_reached = True
-
-    #Current rotation
-    rotationnumber = 0
-
-    #Number of wheel rotations needed to get there
-    number_of_wheel_rotations = distance / 25.13
-
-    while distance_not_reached == True:
-        difference = colorLeft.reflected_light_intensity - colorRight.reflected_light_intensity
-
-        tank.on_for_rotations(20, 20, rotation)
-
-        if difference < 0:
-            #Left is closer to black, We have to turn left
-            if difference > -4:
-                tank.turn_right(20, 1)
-            elif difference < -30:
-                tank.turn_right(20, 4)
-            elif difference < -20:
-                tank.turn_right(20, 3)
-            elif difference > - 10:
-                tank.turn_right(20, 2)
-
-
-        if difference > 0:
-            #Right is closer to black, we have to turn right
-            if difference < 4:
-                tank.turn_left(20, 1)
-            elif difference > 30:
-                tank.turn_left(20, 4)
-            elif difference > 20:
-                tank.turn_left(20, 3)
-            elif difference < 10:
-                tank.turn_left(20, 2)
-
-
-        #Add rotation number
-        rotationnumber += rotation
-
-        #Checks if robot has reached destination
-        if number_of_wheel_rotations < rotationnumber:
-            #Stops the line following program
-            distance_not_reached == False
-
-def turn_degrees(tank, degrees, direction, speed):
-    initial_ang = tank.gyro.angle
-
-
-    if direction == "Right":
-        target_ang = initial_ang + degrees
-        tank.turn_right(speed, degrees)
-
-        sleep(0.5)
-
-        if target_ang != tank.gyro.angle:
-            target_ang -= tank.gyro.angle
-
-            tank.turn_degrees(5, target_ang)
-            sleep(0.5)
-
-    if direction == "Left":
-        target_ang = initial_ang - degrees
-
-        tank.turn_left(speed, degrees)
-
-        sleep(0.5)
-
-        if target_ang != tank.gyro.angle:
-            target_ang -= tank.gyro.angle
-
-            tank.turn_degrees(5, target_ang)
-            sleep(0.5)
 
 def gyro_check (tank, speed, angle):
     angle_now = tank.gyro.angle
@@ -165,12 +50,6 @@ def gyro_check (tank, speed, angle):
     # while tank.gyro.angle != angle:
     #     tank.turn_degrees(5, angle-tank.gyro.angle, True, 0.25)
 
-
-def new_move_incm(tank, speed, distance):
-
-    #Number of wheel rotations needed to get there
-    number_of_wheel_rotations = distance / 25.6353961
-    tank.on_for_rotations(speed, speed, distance / 25.6353961)
 
 def follow_forever(tank, cm, lm):
 
