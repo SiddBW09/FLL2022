7d06897d54b976c7187151829e7e6c2f72bb23e4
LittleLightning <sahana@samrat.us>
2022-11-21 19:14:41 -0800

    all changes

--- PodSA.py
+++ PodSA.py
@@ -40,22 +40,34 @@ def goToMission(tank, fork):
     sleep(0.5)
     tank.on_for_rotations(3, 20, 0.7)  # turn parallel to hydrodam
 
-
-def pushdownThingy():
-    tank = MoveTank(OUTPUT_A, OUTPUT_B)
-    fork = LargeMotor(OUTPUT_C)
-    tank.gyro = GyroSensor(INPUT_1)
+def operateMission(tank, fork):
     tank.gyro.reset()
     fork.reset()
-    fork.on_for_rotations(-60, 0.2)
+    Navigation.distance_goer(tank, 8, -5, 0)
+    fork.on_for_rotations(-60, 0.15)  # lift big bar
+    #fork.on_for_rotations(5, 0.08)
+    sleep(0.3)
+    Navigation.goer_no_gyro(tank, 5, -10)
+    Navigation.goer_no_gyro(tank, 1, 5)
+    #tank.on_for_rotations(-10, -10, 0.16, brake=True, block=True)
+    sleep(0.3)
+    fork.on_for_rotations(10, 0.18)
     return
-    #tank.turn_degrees(5, 90, True, 0.05)
-    home_to_plant_backwards(tank, fork)
-    sleep(1)
-    fork.on_for_rotations(10, 0.45)
+    Navigation.distance_goer(tank, 10, 20, 0)
+    Navigation.distance_goer(tank, 40, 20, -10)
+
+
+def pushdownThingy(tank, fork):
+    tank.gyro.reset()
     fork.reset()
-    fork.on_for_rotations(-5, 0.1)
-    Navigation.distance_goer(tank, 9.25, -5, 90) #forward to mission build (tank, distance, speed, angle)
+    #fork.on_for_rotations(-60, 0.2)
+        #tank.turn_degrees(5, 90, True, 0.05)
+    #home_to_plant_backwards(tank, fork)
+    #sleep(1)
+    #fork.on_for_rotations(10, 0.45)
+    fork.reset()
+    #fork.on_for_rotations(-5, 0.1)
+    Navigation.distance_goer(tank, 9.5, -5, 90) #forward to mission build (tank, distance, speed, angle)
     #Navigation.gyro_check(tank, 5, 0)
     #tank.on_for_rotations(-10, -10, 0.13, brake=True,
                           #block=True)  # forward to mission
@@ -65,9 +77,9 @@ def pushdownThingy():
     #return
     #Navigation.gyro_check(tank, 5, 0)
     #forward 5 go back 3
-    tank.on_for_rotations(-5, -5, 0.35, brake=True, block=True)
+    tank.on_for_rotations(-5, -5, 0.3, brake=True, block=True)
     sleep(0.5)
-    tank.on_for_rotations(5, 5, 0.3)
+    #tank.on_for_rotations(5, 5, 0.3)
     fork.on_for_rotations(10, 0.27)  # hit smaller bar
     fork.on_for_rotations(-10, 0.2)
     sleep(0.3)
@@ -75,8 +87,9 @@ def pushdownThingy():
     tank.on_for_rotations(10, 10, 0.17, brake=True, block=True)
     sleep(0.3)
     fork.on_for_rotations(10, 0.23)  # grip down on innovation piece
-    tank.on_for_rotations(30, 30, 1.8, brake=True,
-                          block=True)  # Back to hydrogen plant
+    Navigation.distance_goer(tank, 30, 20, -10)
+    #tank.on_for_rotations(30, 30, 1.8, brake=True,
+    #                      block=True)  # Back to hydrogen plant
 
 def home_to_plant(tank, fork):
     Navigation.distance_goer(tank, 45, -30, 0)
@@ -86,13 +99,16 @@ def home_to_plant(tank, fork):
     Navigation.distance_goer(tank, 5, 30, -35)
 
 def home_to_plant_backwards(tank, fork):
+    fork.on_for_rotations(5, 0.05)
     originalAngle = tank.gyro.angle
-    Navigation.distance_goer(tank, 45, 25, 0) #going left towards toy factory
+    Navigation.distance_goer(tank, 42, 25, 0) #going left towards toy factory
     #Navigation.gyro_check (tank, 15, 45)
     turnDegree = 45-originalAngle
     tank.turn_degrees(10, turnDegree, True, 0.05)
     sleep(0.5)
-    Navigation.distance_goer(tank, 36, 25, 45) #backwards towards innovation circle area
+    #Navigation.distance_goer(tank, 5, 20, 30)
+    #tank.turn_degrees(10, 15, True, 0.05)
+    Navigation.distance_goer(tank, 30, 25, 45) #backwards towards innovation circle area
     sleep(0.5)
     #Navigation.gyro_check (tank, 10, 90)
     originalAngle = tank.gyro.angle
@@ -107,8 +123,12 @@ def home_to_plant_backwards(tank, fork):
 #goToMission(tank, fork)
 
 if __name__ == "__main__":
+    tank = MoveTank(OUTPUT_A, OUTPUT_B)
+    fork = LargeMotor(OUTPUT_C)
+    tank.gyro = GyroSensor(INPUT_1)
     time1 = time()
-    pushdownThingy()
+    operateMission(tank,fork)
+    #pushdownThingy(tank, fork)
     time2 = time()
     init.debug_print(time2-time1)
 
